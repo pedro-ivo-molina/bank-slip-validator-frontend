a7883778e0296bebc6aa1e3f85f8d5d8a25446bc
Pedro Molina <pedro.ivo.molina@gmail.com>
2021-06-02 23:19:32 -0300

    feat: heroku changes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const path = require('path');
-const express = require('express');
-
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+// const bodyParser = require('body-parser');
+const path = require("path");
 const app = express();
+const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, "build")));
 
-const server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
-});
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import { LeitorDeArquivo } from './Components/LeitorDeArquivo/LeitorDeArquivo.js
 import 'fontsource-roboto';
 
 function App() {
-    return (
-        
-<div className="Header">
-  <div className="Header-background"></div>
-  <div className="Header-content">
-    <div className="Header-hero">
-      <h1>Validador de Arquivos <br/> de Remessa</h1>
-      <LeitorDeArquivo></LeitorDeArquivo>
-      <p>Simples e fácil de usar.</p>
+  return (      
+    <div className="Header">
+      <div className="Header-background"></div>
+      <div className="Header-content">
+        <div className="Header-hero">
+          <h1>Validador de Arquivos <br/> de Remessa</h1>
+          <LeitorDeArquivo></LeitorDeArquivo>
+          <p>Simples e fácil de usar.</p>
+        </div>
+        <div className="Header-visuals">
+        </div>
+      </div>
     </div>
-    <div className="Header-visuals">
-    </div>
-  </div>
-</div>
-    );
+  );
 }
 
 export default App;
